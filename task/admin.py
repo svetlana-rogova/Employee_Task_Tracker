c30e192b0325bab5dd89869935aa4506fc7987c4
Светлана Рогова <mifistofiya@gmail.com>
2026-06-01 20:46:36 +0300

    Add admin and Fix models

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from task.models import Employee, Task
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'middle_name', 'post')
+    search_fields = ('last_name',)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'parent_task', 'period', 'status')
+    search_fields = ('period',)
--- task/models.py
+++ task/models.py
@@ -11,7 +11,7 @@ class Employee(models.Model):
     post = models.CharField(max_length=150, verbose_name='должность')
 
     def __str__(self):
-        return f'{self.first_name} {self.middle_name} {self.last_name}'
+        return f'{self.first_name} {self.middle_name} {self.last_name} {self.post}'
 
     class Meta:
         verbose_name = 'сотрудник'
@@ -36,8 +36,7 @@ class Task(models.Model):
     title = models.CharField(max_length=150, verbose_name='наименование')
     parent_task = models.ForeignKey("self", on_delete=models.SET_NULL, null=True, blank=True,
                                     related_name="children_tasks", verbose_name='ссылка на родительскую задачу')
-    executor = models.ManyToManyField('Employee', max_length=150, verbose_name='исполнитель',
-                                      blank=True, related_name="task")
+    executor = models.ManyToManyField('Employee', verbose_name='исполнитель', blank=True, related_name="task")
     period = models.DateTimeField(verbose_name='срок')
     status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='created', verbose_name='статус')
 
