e0f25f98cf04609873a6cb9cc921987bf5214984
Светлана Рогова <mifistofiya@gmail.com>
2026-06-02 21:19:13 +0300

    Add task for EmployeeSerializers

--- task/serializers.py
+++ task/serializers.py
@@ -2,15 +2,6 @@ from rest_framework import serializers
 from task.models import Employee, Task
 
 
-class EmployeeSerializer(serializers.ModelSerializer):
-    """
-    Сериализатор для сотрудника
-    """
-    class Meta:
-        model = Employee
-        fields = '__all__'
-
-
 class TaskSerializer(serializers.ModelSerializer):
     """
     Сериализатор для задачи
@@ -18,3 +9,14 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для сотрудника
+    """
+    task = TaskSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Employee
+        fields = ['first_name', 'last_name', 'middle_name', 'post', 'task']
