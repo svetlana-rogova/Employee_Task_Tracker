a846886e81d6d0bc0662340191105d75be08760c
Светлана Рогова <mifistofiya@gmail.com>
2026-06-02 21:21:14 +0300

    Add action busy

--- task/views.py
+++ task/views.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
 
 from task.models import Task, Employee
 from task.serializers import TaskSerializer, EmployeeSerializer
+from django.db.models import Count, Q
+from rest_framework.response import Response
 
 
 class TaskViewSet(viewsets.ModelViewSet):
@@ -18,3 +21,13 @@ class EmployeeViewSet(viewsets.ModelViewSet):
     """
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+
+    @action(detail=False, methods=['get'])
+    def busy(self, request):
+        """
+        Эндпоинт для вывода сотрудников с активными задачами и сортировкой по ним
+        """
+        employee_tasks = Employee.objects.filter(task__status='in_progress').annotate(
+            task_count_progress=Count('task')).order_by('-task_count_progress')
+        serializer = EmployeeSerializer(employee_tasks, many=True)
+        return Response(serializer.data)
