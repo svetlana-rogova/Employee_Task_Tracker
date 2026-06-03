9b8272d793415ceaa3c5ce769fe1a856431433eb
Светлана Рогова <mifistofiya@gmail.com>
2026-06-03 22:08:05 +0300

    Add action_tasks for Employee and Fix output for Task

--- task/serializers.py
+++ task/serializers.py
@@ -6,17 +6,43 @@ class TaskSerializer(serializers.ModelSerializer):
     """
     Сериализатор для задачи
     """
+    executor = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='last_name'
+    )
+
+    parent_task = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='title'
+    )
+
+    children_task = serializers.SlugRelatedField(
+        source='children_tasks',
+        many=True,
+        read_only=True,
+        slug_field='title'
+    )
+
     class Meta:
         model = Task
-        fields = '__all__'
+        fields = ['title', 'executor', 'period', 'status', 'children_task', 'parent_task']
+
+
+
+
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
     """
     Сериализатор для сотрудника
     """
-    task = TaskSerializer(many=True, read_only=True)
+    active_tasks = serializers.SerializerMethodField()
 
     class Meta:
         model = Employee
-        fields = ['first_name', 'last_name', 'middle_name', 'post', 'task']
+        fields = ['first_name', 'last_name', 'middle_name', 'post', 'active_tasks']
+
+    def get_active_tasks(self, obj):
+        active_tasks = obj.task.filter(status='in_progress')
+        return [task.title for task in active_tasks]
